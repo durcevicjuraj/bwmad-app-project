8ad8f703de74422302ab5cd3ba4accadddb9fa8e
Juraj Durcevic <juraj.djurcevic@gmail.com>
2024-01-23 15:07:32 +0100

    Create project app

---
+++ yu-gi-oh-cardex/yu-gi-oh-cardex/yu_gi_oh_cardexApp.swift
@@ -0,0 +1,17 @@
+//
+//  yu_gi_oh_cardexApp.swift
+//  yu-gi-oh-cardex
+//
+//  Created by Juraj Đurčević on 23.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct yu_gi_oh_cardexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
